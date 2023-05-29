3d055918a7fc639d27758b534fff9c227d97ba38
Brian Chebon <brynnlamura@gmail.com>
2023-05-29 16:07:11 +0300

    update

---
+++ app/Dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Dashboard() {
+  return (
+    <main>
+      <h1>Welcome to Dashboard</h1>
+    </main>
+  );
+}
